21cc92182fe930d3ce92e4f5a45a85ee023cde94
Kaizan08 <jlsnyder08@gmail.com>
2017-06-10 19:31:15 -0500

    finialized few things to make it work easy way

--- main.js
+++ main.js
@@ -1,27 +1,75 @@
 var button = document.querySelectorAll('.button');
 var output = document.querySelector('.output');
 var clearbtn = document.querySelector('.clear');
-var total="";
-
+var viewer="";
+var number=[];
+var sign="";
 for (var i = 0; i < button.length; i++){
     if (button[i].textContent == '/' || button[i].textContent == '*' || button[i].textContent == '+' || 
-    button[i].textContent == '-'){
+        button[i].textContent == '-'){
         button[i].style.color = '#000000';
-    }
-    // if button[i].innerHTML 
+    } 
     button[i].addEventListener('click', function getValue(e){
         // total += parseInt(e.srcElement.innerHTML, 10);
-        console.log(e);
-        total += e.srcElement.innerHTML;
-        output.textContent = total;
-        return e;
-});
+        if (e.srcElement.innerHTML == '='){
+            number.push((viewer.split(sign))[1]);
+            switch (sign){
+                case "+":
+                    output.textContent = sum(number);
+                    break;
+                case "-":
+                    output.textContent = subtract(number);
+                    break;
+                case "*":
+                    output.textContent = multiply(number);
+                    break;
+                case "/":
+                    output.textContent = divide(number);
+                    break;
+                default:
+                    console.log(sign);
+                    output.textContent = "You did something stupid!";
+            }    
+            return e;        
+        }
+        else if (e.srcElement.innerHTML == "+" || e.srcElement.innerHTML == "-" || e.srcElement.innerHTML == "*" || 
+                e.srcElement.innerHTML == "/"){
+            if (sign == false){
+                sign = e.srcElement.innerHTML;   //gets the symbol to determine function
+                number.push(viewer);
+                viewer += e.srcElement.innerHTML;
+                output.textContent = viewer;
+                return e; 
+            }   else {
+                viewer += e.srcElement.innerHTML;
+                output.textContent = viewer;
+                return e; 
+            }    
+        } else {
+            viewer += e.srcElement.innerHTML;
+            output.textContent = viewer;
+            return e;
+        }
+    });
 }
 
 clearbtn.addEventListener('click', function clear(e){
-    total = "";
+    number = [];
     sign = "";
-    temp = "";
-    output.textContent = total;
+    viewer = "";
+    output.textContent = viewer;
     return e;
 });
+
+function sum(array){
+    return parseInt(array[0], 10)+parseInt(array[1], 10);
+}
+function subtract(array){
+    return parseInt(array[0], 10)-parseInt(array[1], 10);
+}
+function multiply(array){
+    return parseInt(array[0], 10)*parseInt(array[1], 10);
+}
+function divide(array){
+    return parseInt(array[0], 10)/parseInt(array[1], 10);
+}
